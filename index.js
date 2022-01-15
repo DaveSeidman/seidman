b848854ea8703b0dafa37585fff8e5abe96de07b
Dave Seidman <daveseidman@gmail.com>
2022-01-15 12:09:48 -0500

    conversation cycle

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const inquirer = require('inquirer');
 const open = require('open');
 const { NlpManager, ConversationContext } = require('node-nlp');
+const colors = require('colors');
 const { projects } = require('./projects.json');
 
 class App {
   constructor() {
+    this.continueChat = this.continueChat.bind(this);
     this.manager = new NlpManager({ languages: ['en'] });
     this.manager.load();
     this.context = new ConversationContext();
 
-    this.welcome();
+    // this.welcome();
+    this.startChat();
   }
 
   welcome() {
@@ -70,20 +73,21 @@ class App {
           ],
         },
       ]).then((answers) => {
-        // console.log('here', answers.welcome);
-        // console.log(answers.projectType);
         const filteredProjects = projects.filter(project => project.type.indexOf(answers.projectType) >= 0);
+        filteredProjects.push({ name: 'Go Back ⤴', value: 'back' });
         if (filteredProjects.length) {
           inquirer.prompt([
             {
+              message: `Here are some of my ${answers.projectType} projects:`,
               name: 'projectLink',
               type: 'list',
               choices: filteredProjects,
             },
-          ]).then((answers) => {
+          ]).then(({ projectLink }) => {
+            if (projectLink === 'back') return this.welcome();
             console.log('okay, I\'ll open that one for you');
             setTimeout(() => {
-              open(`https://daveseidman.com/${answers.projectLink}`);
+              open(`https://daveseidman.com/${projectLink}`);
             }, 2000);
           });
         } else {
@@ -102,152 +106,21 @@ class App {
 
   startChat() {
     console.log('start chat');
-    inquirer.prompt([{ name: 'opening', message: 'cool, ask me anything' }]).then((answers) => {
-      // console.log(answers.opening);
-      this.manager.process('en', answers.opening, this.context).then((res) => {
-        console.log(res.answer);
-        // console.log(this.context);
-        this.continueChat(res);
-      });
+    inquirer.prompt([{ name: 'start', message: 'cool, ask me anything' }]).then((answers) => {
+      this.manager.process('en', answers.start, this.context).then(this.continueChat);
     });
   }
 
   continueChat(res) {
-    // console.log(res);
-    // inquirer.prompt
-    // this should keep calling continue and eventually endChat
+    if (res.intent === 'greetings.bye') return this.endChat();
+    inquirer.prompt([{ name: 'continue', message: res.answer }]).then((answers) => {
+      this.manager.process('en', answers.continue, this.context).then(this.continueChat);
+    });
   }
 
   endChat() {
-
+    console.log('okay, see ya soon. You can type `npm start` to talk to me again.');
   }
 }
 
 const app = new App();
-
-
-// const CliGui = require('cligui2');
-// const open = require('open');
-//
-// const iface = new CliGui();
-//
-// const respond = (type) => {
-//   iface.log(`lets talk about ${type}`);
-// };
-//
-// iface.list('what kinds of projects are you interested in?', {
-//   AR: () => { respond('ar'); },
-//   VR: () => { respond('vr'); },
-// });
-//
-// const disciplines = ['Full Stack Dev', 'XR Design / Dev', 'Game Engine Dev', 'Robotics', 'Machine Learning', 'Physical Computing', 'Holograms / Lenticular', 'Product Design', 'UI / UX Design', '3D Printing', '3D Scanning / Lidar', 'App Development', 'Data Visualization', 'WebSockets', 'CI / CD', 'AWS / GCP', 'Analytics', 'Model View Controller', 'Object-Oriented', 'Accesibility WCAG', 'PCB Design', 'Volumetric Video', 'Render Farming', 'Code Linting', 'Unit Testing', 'Crossbrowser Testing'];
-// const languages = ['JavaScript', 'TypeScript', 'Node', 'Python', 'HTML5 / Canvas', 'WebGL', 'CSS', 'C#', 'SQL', 'PHP', 'Sketch', 'SASS / SCSS'];
-// const projects = ['DaveSeidman', 'NFL AR by Verizon', 'ReFurbished', 'Wedding Website', 'CatDive', 'VR Coding', 'The Claw', 'HoloChat', 'Living Distance', 'Pride Parade', 'Cam Repeater', 'Fakebook', 'Stranger Tees'];
-//
-// const openingLine = 'Follow the white rabbit';
-//
-// // iface.log('Hey there and welcome to my command line portfolio');
-//
-//
-// const work = () => {
-//   iface.log('Sure, have a look at my portfolio');
-//   open('https://daveseidman.com');
-// };
-//
-// const about = () => {
-//   iface.log('let me tell you about myself!');
-// };
-//
-// const question = () => {
-//   // iface.prompt();
-//   iface.stop();
-// };
-//
-// const resume = () => {
-//   iface.log('okay');
-//   open('https://daveseidman.com/resume');
-// };
-//
-// const search = () => {
-//   iface.search('lets run a search', disciplines, () => {
-//     iface.log('okay');
-//   });
-// };
-//
-// const quit = () => {
-//   iface.stop();
-// };
-//
-// iface.list('What would you like to do?', {
-//   'Show me your work': work,
-//   'Ask me a question': question,
-//   'Tell me about yourself': about,
-//   'View your resume': resume,
-//   'Search your skillset': search,
-//   Quit: quit,
-// });
-// interface.gprompt("this is a guided prompt","type something",options,function(main,output) {
-//   interface.log(output);
-// })
-
-// const main = (e) => {
-//   interface.log('here', e);
-// }
-//
-// interface.list("This is a list",["a","b","c","d"],function(main,chosen) {
-//
-// })
-//
-// interface.list("Another way",["a","b","c"],[
-//   function(main) {},
-//   function(main) {},
-//   function(main) {}
-// ])
-//
-// interface.list("Again Another way",[
-//   {
-//     name: "a",
-//     call: function(main) {}
-//   },
-//   {
-//     name: "b",
-//     call: function(main) {}
-//   }
-// ])
-//
-// interface.list("The fourth way",{
-//   a: function(main) {},
-//   b: function(main) {},
-//   c: function(main) {}
-// })
-
-
-/*
-const prompt = require('prompt');
-const figlet = require('figlet');
-const colors = require('colors/safe');
-
-
-prompt.message = colors.blue('\nThanks for installing my module: CLI-Mate!\n\n');
-prompt.delimiter = '\n';
-
-prompt.start();
-
-prompt.get({
-  properties: {
-    name: {
-      description: colors.blue('Tell me your name and I\'ll do something cool:'),
-      hidden: true,
-    },
-  },
-}, (promptError, promptResult) => {
-  if (!promptError) {
-    figlet(promptResult.name, (figletError, figletResult) => {
-      if (!figletError) {
-        console.log(figletResult);
-        prompt.stop();
-      }
-    });
-  }
-});
-*/
