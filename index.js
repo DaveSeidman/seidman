f8232972e2b11638a496e0961b98af6afdd98d64
Dave Seidman <daveseidman@gmail.com>
2022-01-17 16:26:50 -0500

    updates

--- index.js
+++ index.js
@@ -1,40 +1,79 @@
 const inquirer = require('inquirer');
 const open = require('open');
 const { NlpManager, ConversationContext } = require('node-nlp');
-const colors = require('colors');
+// const colors = require('colors');
+// const blessed = require('blessed');
+const { terminal } = require('terminal-kit');
 const { projects } = require('./projects.json');
 
 class App {
   constructor() {
     this.continueChat = this.continueChat.bind(this);
+    this.openProjects = this.openProjects.bind(this);
+    this.welcome = this.welcome.bind(this);
+
     this.manager = new NlpManager({ languages: ['en'] });
     this.manager.load();
     this.context = new ConversationContext();
 
-    // this.welcome();
-    this.startChat();
+    this.welcome2();
+    // this.startChat();
+  }
+
+  type(string, options) {
+    const defaults = {
+      speed: 100,
+      variation: 0,
+    };
+    options = Object.assign(defaults, options);
+    console.log(options);
+    return new Promise((resolve) => {
+      for (let i = 0; i < string.length; i += 1) {
+        setTimeout(() => {
+          process.stdout.write(string.charAt(i));
+        }, (i * options.speed) + Math.random() * options.variation);
+      }
+
+      setTimeout(() => resolve(), string.length * options.speed);
+    });
+  }
+
+  welcome2() {
+    // terminal('hello world');
+    // console.clear();
+    // process.stdin.on('data', (data) => {
+    //   console.log(`hey ${data.toString()}`);
+    // });
+    this.type('hello world', { speed: 70, variation: 300 });
   }
 
   welcome() {
     return new Promise((resolve) => {
-      inquirer.prompt([
-        {
-          name: 'welcome',
-          message: 'Hi! Welcome to the command line version of my portfolio. \n What would you like to do? (to exit hit cntl+c, to restart type `npm start`)',
-          type: 'list',
-          choices: [
-            { name: 'Talk with me', value: 'startChat' },
-            { name: 'Open Portfolio', value: 'openPortfolio' },
-            { name: 'Open Resume', value: 'openResume' },
-            { name: 'View Projects', value: 'openProjects' },
-          ],
-        },
-      ]).then((answers) => {
-        this[answers.welcome]();
-        return resolve();
-      }).catch((error) => {
-        console.log('error', error);
-      });
+      console.clear();
+      // console.log('Follow the white rabbit...');
+
+
+      setTimeout(() => {
+        console.clear();
+        setTimeout(() => {
+          inquirer.prompt([
+            {
+              name: 'welcome',
+              message: 'Hi! Welcome to the command line version of my portfolio. \n What would you like to do? (to exit hit cntl+c, to restart type `npm start`)',
+              type: 'list',
+              choices: [
+                { name: 'Talk with me', value: 'startChat' },
+                { name: 'Open Portfolio', value: 'openPortfolio' },
+                { name: 'Open Resume', value: 'openResume' },
+                { name: 'View Projects', value: 'openProjects' },
+              ],
+            },
+          ]).then((answers) => {
+            this[answers.welcome]();
+            return resolve();
+          });
+        }, 1000);
+      }, 1000);
     });
   }
 
@@ -42,14 +81,14 @@ class App {
     console.log('Sure, let me pull that up for you in a browser');
     setTimeout(() => {
       open('https://daveseidman.com/resume');
-    }, 2000);
+    }, 1000);
   }
 
   openPortfolio() {
     console.log("Sounds good, I'm going ot try and open my portfolio in your default browser!");
     setTimeout(() => {
       open('https://daveseidman.com');
-    }, 2000);
+    }, 1000);
   }
 
   openProjects() {
@@ -70,12 +109,13 @@ class App {
             { name: 'Physical Computing', value: 'PC' },
             { name: 'Experimental', value: 'EXP' },
             { name: 'Live Events', value: 'LIV' },
+            { name: 'Go Back ⤴', value: 'back' },
           ],
         },
       ]).then((answers) => {
         const filteredProjects = projects.filter(project => project.type.indexOf(answers.projectType) >= 0);
         filteredProjects.push({ name: 'Go Back ⤴', value: 'back' });
-        if (filteredProjects.length) {
+        if (filteredProjects.length > 1) {
           inquirer.prompt([
             {
               message: `Here are some of my ${answers.projectType} projects:`,
@@ -88,7 +128,7 @@ class App {
             console.log('okay, I\'ll open that one for you');
             setTimeout(() => {
               open(`https://daveseidman.com/${projectLink}`);
-            }, 2000);
+            }, 1000);
           });
         } else {
           console.log('sorry, no matches');
@@ -108,19 +148,46 @@ class App {
     console.log('start chat');
     inquirer.prompt([{ name: 'start', message: 'cool, ask me anything' }]).then((answers) => {
       this.manager.process('en', answers.start, this.context).then(this.continueChat);
+    }).catch((error) => {
+      console.log('caught the err 1');
     });
   }
 
   continueChat(res) {
-    if (res.intent === 'greetings.bye') return this.endChat();
-    inquirer.prompt([{ name: 'continue', message: res.answer }]).then((answers) => {
+    this.routes = {
+      // 'None': { call: this.fallback },
+      'greetings.bye': { call: this.endChat },
+      'fallback.reset': { call: this.reset },
+      'work.kind': { call: this.openProjects },
+    };
+    // if (res.intent === 'greetings.bye') return this.endChat();
+    if (this.routes[res.intent]) return this.routes[res.intent].call();
+    const name = 'continue';
+    const message = res.intent === 'None' ? 'Sorry, I did\'nt understand.' : res.answer;
+    inquirer.prompt([{ name, message }]).then((answers) => {
       this.manager.process('en', answers.continue, this.context).then(this.continueChat);
+    }).catch((error) => {
+      console.log('caught the err 2');
     });
   }
 
   endChat() {
     console.log('okay, see ya soon. You can type `npm start` to talk to me again.');
   }
+
+  reset() {
+    console.log('resetting');
+  }
+  //
+  // fallback() {
+  //
+  // }
 }
 
 const app = new App();
+
+process.on('SIGINT', () => {
+  console.log('Caught interrupt signal');
+
+  process.exit();
+});
